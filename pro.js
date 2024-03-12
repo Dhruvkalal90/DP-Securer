c424cfecc3da42ce73a5b77c2d830d904c30b1f8
Dhruvkalal90 <163150881+Dhruvkalal90@users.noreply.github.com>
2024-03-12 20:25:39 +0530

    pro.js

--- pro.js
+++ pro.js
@@ -23,25 +23,43 @@ function checkpass()
 
 function setpass()
 {
-    var pass1=document.getElementById("pass1").value;
-    var pass2=document.getElementById("pass2").value;
-    if (pass1===pass2)
+    var password=localStorage.getItem("password")
+    if (password)
     {
-        localStorage.setItem("password",pass2);
-        alert("password Set!");
-        window.location.href="index.html";
+        alert("Password already set!")
     }
-    else if(pass1!=pass2)
-    {
-        document.getElementById("target").innerHTML="Password doesn't matched!";
-        return false;
+    if(!password){
+
+        var pass1=document.getElementById("pass1").value;
+        var pass2=document.getElementById("pass2").value;
+        if (pass1===pass2)
+        {
+            localStorage.setItem("password",pass2);
+            alert("password Set!");
+            window.location.href="index.html";
+        }
+        else if(pass1!=pass2)
+        {
+            document.getElementById("target").innerHTML="Password doesn't matched!";
+            return false;
+        }
     }
 }
 
 function resetpass()
 {
-    localStorage.removeItem("password");
-    alert("Password deleted\nYou can set new password");
+    var password=localStorage.getItem("password");
+    var old=document.getElementById("old").value;
+    if(old===password)
+    {
+        localStorage.removeItem("password");
+        alert("Password deleted\nYou can set new password");
+    }
+    else if(old!=password)
+    {
+        alert("Wrong password");
+    }
+    
 }
 
 function funmen(x) {
